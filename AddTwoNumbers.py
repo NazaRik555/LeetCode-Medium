724b347e14309d969e95e82df4d4f763b8776d88
Nazar <pankov.n2015@yandex.ru>
2024-05-22 20:34:15 +0300

    3 tests passed

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -1,16 +1,21 @@
+def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        newList = ListNode()
+        node = newList
+        C = 0
+        
+        while l1 is not None or l2 is not None or C != 0:
+            x = l1.val if l1 is not None else 0
+            y = l2.val if l2 is not None else 0
+            print (x,y)
+            sum = x+y+C
+            C = sum//10
+            
+            node.next = ListNode(sum%10)
+            node = node.next
 
-def addTwoNumbers(l1,l2):
-        lst1 = ''
-        lst2 = ''
-        l1.reverse()
-        l2.reverse()
-        for el1, el2 in zip(l1, l2):
-            lst1+=str(el1)
-            lst2+=str(el2)
-        a = int(lst1)+int(lst2)
-        a = str(a)
-        print (''.join(reversed(a)))
-
-l1 = [2,4,3]
-l2 = [5,6,4]
-addTwoNumbers(l1,l2)
+            l1 = l1.next if l1 is not None else None
+            l2 = l2.next if l2 is not None else None
+        print (newList)
+        result = newList.next
+        newList.next = None
+        return result
